7f80241d1e236d60a848c7c885e1fb62a767281e
chrislo27 <kinchrislo@gmail.com>
2016-04-15 16:48:38 -0700

    Implements QuadRectangleable

--- core/src/chrislo27/kilojoule/core/entity/Entity.java
+++ core/src/chrislo27/kilojoule/core/entity/Entity.java
@@ -1,6 +1,35 @@
 package chrislo27.kilojoule.core.entity;
 
+import com.badlogic.gdx.math.Rectangle;
 
-public class Entity {
+import ionium.util.quadtree.QuadRectangleable;
+
+public abstract class Entity implements QuadRectangleable {
+
+	public Rectangle boundingBox = new Rectangle();
+
+	public Entity(float x, float y, float width, float height) {
+		boundingBox.set(x, y, width, height);
+	}
+
+	@Override
+	public float getX() {
+		return boundingBox.x;
+	}
+
+	@Override
+	public float getY() {
+		return boundingBox.y;
+	}
+
+	@Override
+	public float getWidth() {
+		return boundingBox.width;
+	}
+
+	@Override
+	public float getHeight() {
+		return boundingBox.height;
+	}
 
 }
