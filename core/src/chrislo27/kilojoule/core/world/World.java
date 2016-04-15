bc311e6889f9d904b8dd70ed0f793aa6286f2c77
chrislo27 <kinchrislo@gmail.com>
2016-04-15 07:57:28 -0700

    Added dimension map

--- core/src/chrislo27/kilojoule/core/world/World.java
+++ core/src/chrislo27/kilojoule/core/world/World.java
@@ -1,7 +1,15 @@
 package chrislo27.kilojoule.core.world;
 
+import com.badlogic.gdx.utils.ObjectMap;
+
+import chrislo27.kilojoule.core.dimension.Dimension;
 
 public class World {
 
-	
+	public ObjectMap<String, Dimension> dimensions = new ObjectMap<>();
+
+	public World() {
+
+	}
+
 }
