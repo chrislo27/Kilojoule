5c24eeba418352197566f441b3f0e36fd6cca989
chrislo27 <kinchrislo@gmail.com>
2016-04-15 18:26:51 -0700

    Added world renderer

---
+++ core/src/chrislo27/kilojoule/client/render/WorldRenderer.java
@@ -0,0 +1,61 @@
+package chrislo27.kilojoule.client.render;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
+
+import chrislo27.kilojoule.core.block.Block;
+import chrislo27.kilojoule.core.dimension.Dimension;
+import chrislo27.kilojoule.core.world.World;
+
+public class WorldRenderer {
+
+	public static int extraMargin = 1;
+
+	private final World world;
+	public OrthographicCamera camera;
+
+	public WorldRenderer(World world) {
+		this.world = world;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1280, 720);
+	}
+
+	public void render(Batch batch) {
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
+		batch.begin();
+
+		Dimension currentDim = world.getCurrentDim();
+
+		int minX = (int) MathUtils.clamp(
+				camera.position.x - camera.viewportWidth * 0.5f - extraMargin, 0,
+				currentDim.dimWidth);
+		int minY = (int) MathUtils.clamp(
+				camera.position.y - camera.viewportHeight * 0.5f - extraMargin, 0,
+				currentDim.dimHeight);
+		int maxX = (int) MathUtils.clamp(
+				camera.position.x + camera.viewportWidth * 0.5f + extraMargin, 0,
+				currentDim.dimWidth);
+		int maxY = (int) MathUtils.clamp(
+				camera.position.y + camera.viewportHeight * 0.5f + extraMargin, 0,
+				currentDim.dimHeight);
+
+		Block b;
+		for (int x = minX; x < maxX; x++) {
+			for (int y = minY; x < maxY; y++) {
+				b = currentDim.getBlock(x, y);
+
+				if (b == null) continue;
+				if (b.getRenderBlock() == null) continue;
+
+				b.getRenderBlock().render(batch, currentDim, x, y);
+			}
+		}
+
+		batch.end();
+	}
+
+}
