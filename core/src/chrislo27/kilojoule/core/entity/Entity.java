7005e5e1ca50799f7d13554ff59344d44488fcfa
chrislo27 <kinchrislo@gmail.com>
2016-04-15 17:18:26 -0700

    Added velocity

--- core/src/chrislo27/kilojoule/core/entity/Entity.java
+++ core/src/chrislo27/kilojoule/core/entity/Entity.java
@@ -1,12 +1,14 @@
 package chrislo27.kilojoule.core.entity;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 import ionium.util.quadtree.QuadRectangleable;
 
 public abstract class Entity implements QuadRectangleable {
 
 	public Rectangle boundingBox = new Rectangle();
+	public Vector2 velocity = new Vector2(0, 0);
 
 	public Entity(float x, float y, float width, float height) {
 		boundingBox.set(x, y, width, height);
