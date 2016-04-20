e3ce06b48f5a561038649764ff99f1801f7aaa59
chrislo27 <kinchrislo@gmail.com>
2016-04-19 22:02:16 -0700

    Fixed duplicate continuous step

--- core/src/chrislo27/kilojoule/core/generation/step/OldHeightmapStep.java
+++ core/src/chrislo27/kilojoule/core/generation/step/OldHeightmapStep.java
@@ -17,26 +17,21 @@ public class OldHeightmapStep extends Step {
 
 	@Override
 	public void step(World world, WorldLoadingBuffer buffer) {
+		double height = world.simplexNoise.eval(x * 0.0325f, 0);
+		int actualHeight = (int) (generator.settings.seaLevel
+				+ (height * generator.settings.hillHeight));
 
-		long time = System.nanoTime();
-		while (System.nanoTime() - time < 15_000_000 && !isFinished()) {
-			double height = world.simplexNoise.eval(x * 0.0325f, 0);
-			int actualHeight = (int) (generator.settings.seaLevel
-					+ (height * generator.settings.hillHeight));
-
-			for (int y = 0; y < actualHeight; y++) {
-				world.setBlock(Blocks.getBlock("dirt"), x, actualHeight);
-			}
-
-			buffer.fillRect(132 / 255f, 84 / 255f, 50 / 255f, x, 0, 1, actualHeight);
-
-			world.setBlock(Blocks.getBlock("grass"), x, actualHeight);
-			buffer.setPixel(0.25f, 1f, 0.25f, x, actualHeight);
-			
-			setPercentage((x * 1f) / world.worldWidth);
-			x++;
+		for (int y = 0; y < actualHeight; y++) {
+			world.setBlock(Blocks.getBlock("dirt"), x, actualHeight);
 		}
 
+		buffer.fillRect(132 / 255f, 84 / 255f, 50 / 255f, x, 0, 1, actualHeight);
+
+		world.setBlock(Blocks.getBlock("grass"), x, actualHeight);
+		buffer.setPixel(0.25f, 1f, 0.25f, x, actualHeight);
+
+		setPercentage((x * 1f) / world.worldWidth);
+		x++;
 	}
 
 	@Override
