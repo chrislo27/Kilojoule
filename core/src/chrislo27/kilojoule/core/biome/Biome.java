252e4c01fe1c430bc10ae47ce79ce49cecaeba88
chrislo27 <kinchrislo@gmail.com>
2016-04-21 17:02:22 -0700

    Added hill coefficient

--- core/src/chrislo27/kilojoule/core/biome/Biome.java
+++ core/src/chrislo27/kilojoule/core/biome/Biome.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Color;
 public class Biome {
 
 	public Color foliageColor = new Color();
+	public float hillCoefficient = 1;
 
 	public Biome(Color foliage) {
 		foliageColor = new Color(foliage.r, foliage.g, foliage.b, 1);
