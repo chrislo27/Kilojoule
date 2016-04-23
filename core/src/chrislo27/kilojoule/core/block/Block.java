a770321cdfe3b37cc94c3d3d1826194a4d899622
chrislo27 <kinchrislo@gmail.com>
2016-04-23 14:27:43 -0700

    Added map color

--- core/src/chrislo27/kilojoule/core/block/Block.java
+++ core/src/chrislo27/kilojoule/core/block/Block.java
@@ -1,15 +1,18 @@
 package chrislo27.kilojoule.core.block;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ObjectMap;
 
 import chrislo27.kilojoule.client.render.block.BlockRenderer;
+import chrislo27.kilojoule.core.world.World;
 
 public abstract class Block {
 
 	public static final int TILE_SIZE = 32;
 
 	protected BlockRenderer renderBlock;
+	protected Color mapColor = new Color(1, 1, 1, 1);
 
 	public Block() {
 
@@ -17,6 +20,10 @@ public abstract class Block {
 
 	public BlockRenderer getRenderBlock() {
 		return renderBlock;
+	}
+
+	public Color getMapColor(World world, int x, int y) {
+		return mapColor;
 	}
 
 	public abstract void getRequiredTextures(ObjectMap<String, Texture> map);
