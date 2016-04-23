010c3426bd3ce7755a46c9533971a8034424235e
chrislo27 <kinchrislo@gmail.com>
2016-04-23 15:08:37 -0700

    Added biome interpolation

--- core/src/chrislo27/kilojoule/core/generation/step/height/FineHeightmapStep.java
+++ core/src/chrislo27/kilojoule/core/generation/step/height/FineHeightmapStep.java
@@ -1,9 +1,11 @@
 package chrislo27.kilojoule.core.generation.step.height;
 
 import chrislo27.kilojoule.client.screen.GenerationScreen.WorldLoadingBuffer;
+import chrislo27.kilojoule.core.biome.Biome;
 import chrislo27.kilojoule.core.generation.WorldGenerator;
 import chrislo27.kilojoule.core.generation.step.Step;
 import chrislo27.kilojoule.core.world.World;
+import ionium.templates.Main;
 import ionium.util.MathHelper;
 import ionium.util.i18n.Localization;
 
@@ -26,15 +28,61 @@ public class FineHeightmapStep extends Step {
 		double detail = lastStep.getValue(lastStep.detail, x);
 
 		double actualHeight = (elevation + (roughness * detail)) / (lastStep.heightsFactor * 2f)
-				+ generator.settings.seaLevel;
+				* world.getBiome(x).hillCoefficient + generator.settings.seaLevel;
 
 		actualHeights[x] = actualHeight;
 
-		float color = (x % lastStep.interval == 0 ? 0.4f : 0.25f);
-		buffer.fillRect(color, color, color, x, 0, 1, (int) actualHeight);
+		drawHeight(x, actualHeight, buffer);
 
 		x++;
 		setPercentage((x * 1f) / world.worldWidth);
+
+		if (getPercentage() >= 1) {
+			smoothBiomeBorders(world, buffer);
+		}
+	}
+
+	private void drawHeight(int x, double actualHeight, WorldLoadingBuffer buffer) {
+		float color = (x % lastStep.interval == 0 ? 0.4f : 0.25f);
+		buffer.fillRect(color, color, color, x, 0, 1, (int) actualHeight);
+	}
+
+	private void smoothBiomeBorders(World world, WorldLoadingBuffer buffer) {
+		if (lastStep.interval <= 1) return;
+
+		int x = 0;
+		Biome lastBiome = world.getBiome(0);
+
+		while (x < world.worldWidth) {
+
+			Biome current = world.getBiome(x);
+
+			if (current != lastBiome) {
+				int pointOfChange = x;
+				int left = Math.max(0, x - lastStep.interval);
+				int right = Math.min(actualHeights.length - 1, x + lastStep.interval - 1);
+				double leftHeight = actualHeights[left];
+				double rightHeight = actualHeights[right];
+
+				for (int i = left; i <= right; i++) {
+					actualHeights[i] = MathHelper.lerp(leftHeight, rightHeight,
+							((i - left) * 1f) / (right - left));
+
+					drawHeight(i, actualHeights[i], buffer);
+				}
+
+				buffer.fillRect(0, 0, 0, pointOfChange, 0, 1, world.worldHeight);
+				buffer.fillRect(0.8f, 0.25f, 0.25f, pointOfChange, 0, 1,
+						(int) actualHeights[pointOfChange]);
+
+			}
+
+			lastBiome = current;
+
+			x++;
+
+		}
+
 	}
 
 	@Override
@@ -46,6 +94,10 @@ public class FineHeightmapStep extends Step {
 		if (lastStep instanceof RoughHeightmapStep) {
 			this.lastStep = (RoughHeightmapStep) lastStep;
 			actualHeights = new double[this.generator.world.worldWidth];
+
+			for (int i = 0; i < actualHeights.length; i++) {
+				actualHeights[i] = -999;
+			}
 		} else {
 			throw new IllegalArgumentException("Previous step must be of RoughHeightmapStep");
 		}
