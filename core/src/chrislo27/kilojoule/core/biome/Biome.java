aa9c9275342bf14a15705d19a6af5462b9837bda
chrislo27 <kinchrislo@gmail.com>
2016-04-23 15:36:25 -0700

    Added BiomeGeneratorSettings

--- core/src/chrislo27/kilojoule/core/biome/Biome.java
+++ core/src/chrislo27/kilojoule/core/biome/Biome.java
@@ -2,10 +2,13 @@ package chrislo27.kilojoule.core.biome;
 
 import com.badlogic.gdx.graphics.Color;
 
+import chrislo27.kilojoule.core.generation.BiomeGeneratorSettings;
+
 public class Biome {
 
 	public Color foliageColor = new Color();
 	public float hillCoefficient = 1;
+	public BiomeGeneratorSettings generatorSettings = new BiomeGeneratorSettings();
 
 	public Biome(Color foliage) {
 		foliageColor = new Color(foliage.r, foliage.g, foliage.b, 1);
---
+++ core/src/chrislo27/kilojoule/core/generation/BiomeGeneratorSettings.java
@@ -0,0 +1,6 @@
+package chrislo27.kilojoule.core.generation;
+
+
+public class BiomeGeneratorSettings {
+
+}
