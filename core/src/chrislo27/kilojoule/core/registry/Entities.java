845c1f50deaf830908cc753ddca16b2b25931d17
chrislo27 <kinchrislo@gmail.com>
2016-04-26 16:24:44 -0700

    Added entities list singleton

---
+++ core/src/chrislo27/kilojoule/core/registry/Entities.java
@@ -0,0 +1,51 @@
+package chrislo27.kilojoule.core.registry;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+import chrislo27.kilojoule.core.entity.Entity;
+import chrislo27.kilojoule.core.entity.EntityPlayer;
+import chrislo27.kilojoule.core.world.World;
+
+public class Entities {
+
+	private static Entities instance;
+
+	private Entities() {
+	}
+
+	public static Entities instance() {
+		if (instance == null) {
+			instance = new Entities();
+			instance.loadResources();
+		}
+		return instance;
+	}
+
+	private HashMap<String, Class<? extends Entity>> allEntities = new HashMap<>();
+
+	private void loadResources() {
+		addEntity("player", EntityPlayer.class);
+	}
+
+	public static Class<? extends Entity> getEntityClass(String id) {
+		return instance().allEntities.get(id);
+	}
+
+	public static Entity getNewInstance(String id, World world, float x, float y) {
+		try {
+			return getEntityClass(id).getConstructor(World.class, Float.class, Float.class)
+					.newInstance(world, x, y);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public void addEntity(String id, Class<? extends Entity> clazz) {
+		instance().allEntities.put(id, clazz);
+	}
+
+}
