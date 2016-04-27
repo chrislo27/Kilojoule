bdcfdda7b106c90c9705cd385b469a7e65c109df
chrislo27 <kinchrislo@gmail.com>
2016-04-26 17:56:23 -0700

    Camera tracking

--- core/src/chrislo27/kilojoule/client/render/WorldRenderer.java
+++ core/src/chrislo27/kilojoule/client/render/WorldRenderer.java
@@ -3,28 +3,43 @@ package chrislo27.kilojoule.client.render;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 
 import chrislo27.kilojoule.core.block.Block;
+import chrislo27.kilojoule.core.entity.EntityPlayer;
 import chrislo27.kilojoule.core.world.World;
-import ionium.templates.Main;
 
 public class WorldRenderer {
 
 	public static int extraMargin = 1;
 
 	public OrthographicCamera camera;
+	private Vector3 tempVector = new Vector3();
 
 	public WorldRenderer() {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 26.5f, 15);
 	}
 
-	public void render(Batch batch, World world) {
+	protected void updateCamera(World world) {
+		EntityPlayer player = world.universe.player;
+
+		if (player != null) {
+			tempVector.set(player.physicsBody.bounds.x, player.physicsBody.bounds.y,
+					camera.position.z);
+
+			camera.position.lerp(tempVector, 0.5f);
+		}
+
 		camera.position.x = MathUtils.clamp(camera.position.x, camera.viewportWidth * 0.5f,
 				world.worldWidth - camera.viewportWidth * 0.5f);
 		camera.position.y = MathUtils.clamp(camera.position.y, camera.viewportHeight * 0.5f,
 				world.worldHeight - camera.viewportHeight * 0.5f);
 		camera.update();
+	}
+
+	public void render(Batch batch, World world) {
+		updateCamera(world);
 
 		batch.setProjectionMatrix(camera.combined);
 
